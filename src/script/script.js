517baebea1e0bdccb6595b9e02ef317576329f6d
erikbehrami <ebehrami14@gmail.com>
2024-12-12 15:03:20 +0100

    feature/home-update

--- src/script/script.js
+++ src/script/script.js
@@ -5,18 +5,22 @@ const login = document.querySelector('.login');
 const sregister = document.querySelector('.sregister');
 const register = document.querySelector('.register');
 const signbutton = document.querySelector('.signbutton');
-const body = document.querySelector('.body');
+const body = document.querySelector('body');
+const all = document.querySelector('.all');
 sign.addEventListener('click', function(){
     login.classList.add('login-special');
     body.classList.add('body-special');
+    all.classList.add('all-special');
 })
 iks.addEventListener('click', function(){
     login.classList.remove('login-special');
     body.classList.remove('body-special');
+    all.classList.remove('all-special');
 })
 iks1.addEventListener('click', function(){
     register.classList.remove('register-special');
     body.classList.remove('body-special');
+    all.classList.remove('all-special');
 })
 sregister.addEventListener('click', function(){
     login.classList.remove('login-special');
