0b12ec462bd96d6186d60f1bedd6f9c7103515b0
BlertinH <blertin.hamza@student.uni-pr.edu>
2025-01-02 17:45:20 +0100

    feature/update

--- src/script/script.js
+++ src/script/script.js
@@ -31,3 +31,29 @@ signbutton.addEventListener('click', function(){
     register.classList.remove('register-special');
 })
 
+
+
+let count = 0;
+
+document.getElementById("decreasebtn").onclick = function() {
+    count -= 1;
+    if(count<0)
+    {
+      return count=0;
+    }
+    else
+    {
+        document.getElementById("countLabel").innerHTML = count;
+    }
+   
+}
+
+document.getElementById("resetbtn").onclick = function() {
+    count = 0;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById("increasebtn").onclick = function() {
+    count += 1;
+    document.getElementById("countLabel").innerHTML = count;  // Corrected here
+}
