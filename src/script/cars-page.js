b6559c1c6c16cbaf678e092948ea61582e0b6046
BlertinH <blertin.hamza@student.uni-pr.edu>
2025-01-04 01:07:10 +0100

    CAR-PAGE/UPDEATE

--- src/script/cars-page.js
+++ src/script/cars-page.js
@@ -10,3 +10,14 @@ prev.addEventListener('click', function(){
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide20').prepend(items[items.length - 1]) 
 })
+function rezervo() {
+    document.getElementById("modal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+setTimeout(function() {
+    document.getElementById("modal").style.display = "none";
+});
