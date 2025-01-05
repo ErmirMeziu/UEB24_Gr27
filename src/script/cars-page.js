4de269e80cdc819743a562dbf785fa2823109628
erikbehrami <ebehrami14@gmail.com>
2025-01-05 20:03:29 +0100

    feature/update

--- src/script/cars-page.js
+++ src/script/cars-page.js
@@ -1,14 +1,14 @@
 let next = document.querySelector('.next2')
 let prev = document.querySelector('.prev2')
 
-next.addEventListener('click', function(){
+next.addEventListener('click', function () {
     let items = document.querySelectorAll('.item')
     document.querySelector('.slide20').appendChild(items[0])
 })
 
-prev.addEventListener('click', function(){
+prev.addEventListener('click', function () {
     let items = document.querySelectorAll('.item')
-    document.querySelector('.slide20').prepend(items[items.length - 1]) 
+    document.querySelector('.slide20').prepend(items[items.length - 1])
 })
 function rezervo() {
     document.getElementById("modal").style.display = "block";
@@ -18,44 +18,43 @@ function closeModal() {
     document.getElementById("modal").style.display = "none";
 }
 
-setTimeout(function() {
+setTimeout(function () {
     document.getElementById("modal").style.display = "none";
 });
 
 
 function formatDate(date) {
     const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
-    return date.toLocaleDateString('en-GB', options); 
+    return date.toLocaleDateString('en-GB', options);
 }
 
 function updateDates() {
-   
+
     const pickupInput = document.getElementById('pickup-input').value;
     const dropoffInput = document.getElementById('dropoff-input').value;
 
-  
+
     if (pickupInput && dropoffInput) {
         const pickupDate = new Date(pickupInput);
         const dropoffDate = new Date(dropoffInput);
 
-       
         if (dropoffDate < pickupDate) {
             alert("You selected the wrong date!");
-            return; 
+            return;
         }
 
-      
-        document.getElementById('pickup-date').textContent = formatDate(pickupDate);
-        document.getElementById('dropoff-date').textContent = formatDate(dropoffDate);
+
+        // document.getElementById('pickup-date').textContent = formatDate(pickupDate);
+        // document.getElementById('dropoff-date').textContent = formatDate(dropoffDate);
 
         const durationInMillis = dropoffDate - pickupDate;
-        const durationInDays = durationInMillis / (1000 * 60 * 60 * 24); 
+        const durationInDays = durationInMillis / (1000 * 60 * 60 * 24);
 
         document.getElementById('duration').textContent = `${durationInDays} Day(s)`;
 
         const dailyCost = 64;
-        const totalCost = durationInDays * dailyCost; 
-      
+        const totalCost = durationInDays * dailyCost;
+
         document.getElementById('total-cost').textContent = `${totalCost.toFixed(2)} €`;
     }
 }
